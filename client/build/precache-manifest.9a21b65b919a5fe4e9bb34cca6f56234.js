ca506ef30017ce4a90acd4a7eccaaab1ddfe9578
ognjenvulic <ogi@goodwall.org>
2020-06-05 22:19:20 +0200

    second part

---
+++ client/build/precache-manifest.9a21b65b919a5fe4e9bb34cca6f56234.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "37aea36256376d69d40924d9c86174c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4ec459345da31e11f2cf",
+    "url": "/static/css/main.3b508df1.chunk.css"
+  },
+  {
+    "revision": "c68329d30d9d13d2355a",
+    "url": "/static/js/2.3f68e485.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3f68e485.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4ec459345da31e11f2cf",
+    "url": "/static/js/main.2758ad96.chunk.js"
+  },
+  {
+    "revision": "13c5ce7c89dba05c4fba",
+    "url": "/static/js/runtime-main.8c40394c.js"
+  }
+]);
